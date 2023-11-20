80f1e208953c24d4cf8a956fe2498540bc471ed3
rafiakbar13 <muhammadrafia13@gmail.com>
2023-11-20 18:08:12 +0700

    added image and change type any

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import FavoritesQuotes from '@/components/FavoritesQuotes'
 import KanyeQuotes from '@/components/KanyeQuotes'
 import MyQuotes from '@/components/MyQuotes'
+import Image from 'next/image'
 
 
 export default function Home() {
   return (
-    <main className="max-w-7xl p-10">
+    <main className="max-w-7xl p-10 relative h-screen">
       <div className='flex justify-between w-full gap-x-5'>
         <KanyeQuotes />
         <FavoritesQuotes />
       </div>
       <MyQuotes />
+
+      <div className="absolute z-[-1] bottom-0 left-0 right-0 text-center">
+        <Image src='https://images.businessoffashion.com/profiles/asset/1797/43897e2e4a6d155d72dd9df352017b546ef9e229.jpeg' width={800} height={800} alt='kanye' className="mx-auto" />
+      </div>
     </main>
   )
 }
--- components/MyQuotes.tsx
+++ components/MyQuotes.tsx
@@ -1,39 +1,48 @@
 "use client"
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { setMyQuotes } from '@/features/quoteSlice'
-type Props = {}
+import React, { useState, ChangeEvent, FormEvent } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { setMyQuotes } from '@/features/quoteSlice';
 
-const MyQuotes = (props: Props) => {
-    const dispatch = useDispatch()
-    const [myQuote, setMyQuote] = useState<string>('')
-    const myQuotes: string[] = useSelector((state: any) => state.quote.myQuotes)
-    const handleMyQuote = (e: any) => {
-        setMyQuote(e.target.value)
-    }
+interface MyQuotesProps { }
 
-    const addMyQuote = (e: any) => {
-        e.preventDefault()
-        dispatch(setMyQuotes([...myQuotes, myQuote]))
-        setMyQuote('')
-    }
+const MyQuotes: React.FC<MyQuotesProps> = (props: MyQuotesProps) => {
+    const dispatch = useDispatch();
+    const [myQuote, setMyQuote] = useState<string>('');
+    const myQuotes: string[] = useSelector((state: any) => state.quote.myQuotes);
 
+    const handleMyQuote = (e: ChangeEvent<HTMLInputElement>) => {
+        setMyQuote(e.target.value);
+    };
+
+    const addMyQuote = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        dispatch(setMyQuotes([...myQuotes, myQuote]));
+        setMyQuote('');
+    };
 
     return (
         <section className='mt-10 w-1/2'>
-            <article className='border bg-white  px-4 rounded-lg divide-y-2'>
+            <article className='border bg-white px-4 rounded-lg divide-y-2'>
                 <h2 className='p-2 font-semibold'>Your Quotes</h2>
                 <form action="" className='' onSubmit={addMyQuote}>
                     <div className='flex whitespace-nowrap gap-x-2 py-3'>
-                        <input type="text" name="" id="" className='bg-zinc-300 w-full rounded-md px-3' onChange={handleMyQuote} value={myQuote} placeholder='Add Quotes' />
-                        <button className='bg-black text-white  py-1 rounded-md px-4'>Add Quote</button>
+                        <input
+                            type="text"
+                            name=""
+                            id=""
+                            className='bg-zinc-300 w-full rounded-md px-3'
+                            onChange={handleMyQuote}
+                            value={myQuote}
+                            placeholder='Add Quotes'
+                        />
+                        <button className='bg-black text-white py-1 rounded-md px-4'>Add Quote</button>
                     </div>
                 </form>
                 <div>
                     {myQuotes.length > 0 ? (
                         <ol className='list-decimal px-5'>
                             {myQuotes.map((quote: string, index: number) => (
-                                <li key={index} className=' w-full py-2 rounded-md '>
+                                <li key={index} className='w-full py-2 rounded-md '>
                                     {quote}
                                 </li>
                             ))}
@@ -44,7 +53,7 @@ const MyQuotes = (props: Props) => {
                 </div>
             </article>
         </section>
-    )
-}
+    );
+};
 
-export default MyQuotes
+export default MyQuotes;
